d3212e9945a02c29977b53c97a196bf7db6e741e
crabshank <44329154+crabshank@users.noreply.github.com>
2023-05-30 20:59:08 +0100

    Add files via upload

--- content.js
+++ content.js
@@ -940,96 +940,28 @@ function cl_clk(i) {
 	i.s_vis=chkVis(i,false); //null -> true/false
 	i.c_vis=chkVis(i,true);	
 	let rectV=absBoundingClientRect(i.cvs);
-//event.preventDefault();
-event.stopPropagation();
-let b_pass=true;
-let ct=false;
-	if(event.target===i.clse){
-		i.clse.focus();
-		b_pass=false;
-	}else if(event.target===i.butn){
-		//i.butn.click();
-		btclk(i);
-		b_pass=false;
-	}else if(event.target===i.skb){
-		//i.skb.click();
-		sk_bk(i);
-		b_pass=false;
-	}else if(event.target===i.skf){
-		//i.skf.click();
-		sk_fw(i);
-		b_pass=false;
-	}else if(event.target===i.cvs){
-		i.obscPrg={x: event.pageX, l: rectV.left, w:rectV.width}
-		//i.cvs.click();
-		
+	//event.preventDefault();
+	event.stopPropagation();
+	let b_pass=true;
+	let ct=false;
+	if(event.target===i.cvs){
 		if(cvs_clk>1){
 			cvs_clkd=false;
-			cvs_hdl(event,i,5);
 		}else{
 			cvs_clkd=true;
-			cvs_hdl(event,i,4);
 		}
-
 		b_pass=false;
-	}else if(sk_buff){
-		if(event.target===i.skb_l){
-			//i.skb_l.click();
-			sk_l_bk(i);
-			b_pass=false;
-		}else if(event.target===i.skf_l){
-			//i.skf_l.click();
-			sk_l_fw(i);
-			b_pass=false;
-		}
 	}
 	
 	if(b_pass){ //button not clicked directly
-		
-		let rectC=absBoundingClientRect(i.clse);
-		let rectB=absBoundingClientRect(i.butn);
-		let rectK=absBoundingClientRect(i.skb);
-		let rectF=absBoundingClientRect(i.skf);
-	
-		if(event.pageX >= rectC.left && event.pageX <= rectC.right && event.pageY >= rectC.top && event.pageY <= rectC.bottom){
-			i.clse.focus();
-			ct=true;
-		}else if(event.pageX >= rectB.left && event.pageX <= rectB.right && event.pageY >= rectB.top && event.pageY <= rectB.bottom){
-			//i.butn.click();
-			btclk(i);
-			ct=true;
-		}else if(event.pageX >= rectK.left && event.pageX <= rectK.right && event.pageY >= rectK.top && event.pageY <= rectK.bottom){
-			//i.skb.click();
-			sk_bk(i);
-			ct=true;
-		}else if(event.pageX >= rectF.left && event.pageX <= rectF.right && event.pageY >= rectF.top && event.pageY <= rectF.bottom){
-			//i.skf.click();
-			sk_fw(i);
-			ct=true;
-		}else if(event.pageX >= rectV.left && event.pageX <= rectV.right && event.pageY >= rectV.top && event.pageY <= rectV.bottom){
-			i.obscPrg={x: event.pageX, l: rectV.left, w:rectV.width}
-			//i.cvs.click();
-			if(cvs_clk>1){
-				cvs_clkd=false;
-				cvs_hdl(event,i,5);
-			}else{
-				cvs_clkd=true;
-				cvs_hdl(event,i,4);
-			}
-			ct=true;
-		}else if(sk_buff){
-			let rectKL=absBoundingClientRect(i.skb_l);
-			let rectFL=absBoundingClientRect(i.skf_l);
-			if(event.pageX >= rectKL.left && event.pageX <= rectKL.right && event.pageY >= rectKL.top && event.pageY <= rectKL.bottom){
-				//i.skb_l.click();
-				sk_l_bk(i);
-				ct=true;
-			}else if(event.pageX >= rectFL.left && event.pageX <= rectFL.right && event.pageY >= rectFL.top && event.pageY <= rectFL.bottom){
-				//i.skf_l.click();
-				sk_l_fw(i);
+		if(event.pageX >= rectV.left && event.pageX <= rectV.right && event.pageY >= rectV.top && event.pageY <= rectV.bottom){
+				if(cvs_clk>1){
+					cvs_clkd=false;
+				}else{
+					cvs_clkd=true;
+				}
 				ct=true;
 			}
-		}
 	}
 	def_retCSS(i,false,true);
 	if (b_pass && !ct && i.clse.matches(':focus')){
